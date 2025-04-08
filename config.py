60a2b8ebd2fb0d6a780b2a82d03df35f533826c7
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-08 16:40:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+API_ID = "28744454" #API ID get it from telegram.org
+API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7" #API HASH get it from telegram.org
+BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg" #Bot Token get it from @BotFather
+URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
+#URL_B = "" #News RSS Feed Url 2
+START_PIC = "https://i.ibb.co/b5PwhJxm/photo-2025-04-06-20-48-47-7490897815808245776.jpg" #Startup Pic
+MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority"
+ADMINS = "6266529037" #Admin ID
